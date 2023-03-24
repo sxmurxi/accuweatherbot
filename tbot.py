10b4af9789458c24b025fdc62cfbeabd79ff3449
sxmurxi <aleksandr.tentser@gmail.com>
2023-03-24 17:07:14 +0200

    Merge two functions

--- tbot.py
+++ tbot.py
@@ -1,8 +1,12 @@
+import time
+import logging
+import asyncio
 import requests
+from aiogram import Bot, Dispatcher, executor, types
 def main():
     s_city = "Kyiv"
-    city_id = 0
-    appid = "27801b2265cfd032a3da38480118656e"
+    city_id = 703448
+    appid = "буквенно-цифровой APPID"
     try:
         res = requests.get("http://api.openweathermap.org/data/2.5/find",
                      params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': '27801b2265cfd032a3da38480118656e'})
@@ -26,7 +30,31 @@ def main():
     except Exception as e:
         print("Exception (weather):", e)
         pass
-main()
+def weatherbotcore():
+    main_data = main()
+    TOKEN = "6150401156:AAFggudQIBtiShpS5Ow-PlAhFwx-IxwcWUI"
+    MSG = (main.)
+
+    logging.basicConfig(level=logging.INFO)
+
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher(bot=bot)
 
 
+    @dp.message_handler(commands=["start"])
+    async def start_handler(message: types.Message):
+        user_id = message.from_user.id
+        user_name = message.from_user.first_name
+        user_full_name = message.from_user.full_name
+        logging.info(f'{user_id} {user_full_name} {time.asctime()}')
+        await message.reply(f"Привет, {user_full_name}!")
 
+        for i in range(7):
+            await asyncio.sleep(1)
+            await bot.send_message(user_id, MSG.format(user_name))
+
+
+    if __name__ == "__main__":
+        executor.start_polling(dp)
+
+weatherbotcore()
